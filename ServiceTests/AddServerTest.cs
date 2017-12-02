5b2e9462bab6e41078bdaf79d7b2077806ff5a67
Eburger <kkmegarif@gmail.com>
2017-12-02 23:49:46 +0500

    20170318.

--- ServiceTests/AddServerTest.cs
+++ ServiceTests/AddServerTest.cs
@@ -11,6 +11,26 @@ namespace Kontur.GameStats.Server.ServiceTests
     [TestFixture]
     class AddServerTest
     {
+        [SetUp]
+        public static void InsertData()
+        {
+            using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
+            {
+                var serversCollection = testDatabase.GetCollection<Classes.Server>("servers");
+
+                Classes.Server.AddServerInfo(serversCollection,
+                    new Classes.Server
+                    {
+                        Name = "] My P3rfect Server [",
+                        GameModes = new[] { "DM", "TDM" },
+                        Endpoint = "167.42.23.32-1337"
+                    },
+                    "167.42.23.32-1337");
+
+                serversCollection.EnsureIndex(x => x.Endpoint);
+            }
+        }
+
         [TearDown]
         public static void DropDatabase()
         {
@@ -19,22 +39,16 @@ namespace Kontur.GameStats.Server.ServiceTests
                 testDatabase.DropCollection("servers");
             }
         }
+
         [Test]
         public static void TestServerAddition()
         {
             using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
             {
                 var serversCollection = testDatabase.GetCollection<Classes.Server>("servers");
-                serversCollection.EnsureIndex(x => x.Endpoint);
-
-                var gameModes = new[] { "DM", "TDM" };
-                Classes.Server.AddServerInfo(serversCollection,
-                    new Classes.Server() { Name = "] My P3rfect Server [", GameModes = gameModes, Endpoint = "167.42.23.32-1337" },
-                    "167.42.23.32-1337");
-
                 var resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1337");
                 Assert.AreEqual("] My P3rfect Server [", resultServer.Name);
-                Assert.AreEqual(gameModes, resultServer.GameModes);
+                Assert.AreEqual(new[] { "DM", "TDM" }, resultServer.GameModes);
             }
         }
         [Test]
@@ -43,57 +57,54 @@ namespace Kontur.GameStats.Server.ServiceTests
             using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
             {
                 var serversCollection = testDatabase.GetCollection<Classes.Server>("servers");
-                serversCollection.EnsureIndex(x => x.Endpoint);
-
-                var gameModes = new[] { "DM", "TDM" };
-                Classes.Server.AddServerInfo(serversCollection,
-                    new Classes.Server() { Name = "] My P3rfect Server [", GameModes = gameModes, Endpoint = "167.42.23.32-1337" },
-                    "167.42.23.32-1337");
-
                 var resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1337");
-                Assert.AreEqual("] My P3rfect Server [", resultServer.Name);
-                Assert.AreEqual(gameModes, resultServer.GameModes);
-
-                gameModes = new[] { "CtF", "TS", "DM", "TDM" };
-                Classes.Server.AddServerInfo(serversCollection,
-                    new Classes.Server() { Name = ">> Sniper Heaven <<", GameModes = gameModes, Endpoint = "167.42.23.32-1337" },
-                    "167.42.23.32-1337");
-
-                resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1337");
-                Assert.AreEqual(">> Sniper Heaven <<", resultServer.Name);
-                Assert.AreEqual(gameModes, resultServer.GameModes);
-            }
-        }
-        [Test]
-        public static void TestServerAddition2()
-        {
-            using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
-            {
-                var serversCollection = testDatabase.GetCollection<Classes.Server>("servers");
-                serversCollection.EnsureIndex(x => x.Endpoint);
-
-                var gameModes = new[] { "DM", "TDM" };
-                Classes.Server.AddServerInfo(serversCollection,
-                    new Classes.Server() { Name = "] My P3rfect Server [", GameModes = gameModes, Endpoint = "167.42.23.32-1337" },
-                    "167.42.23.32-1337");
-
-                var resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1337");
-                Assert.AreEqual("] My P3rfect Server [", resultServer.Name);
-                Assert.AreEqual(gameModes, resultServer.GameModes);
-
-                gameModes = new[] { "CtF", "TS", "DM", "TDM" };
-                Classes.Server.AddServerInfo(serversCollection,
-                    new Classes.Server() { Name = ">> Sniper Heaven <<", GameModes = gameModes, Endpoint = "167.42.23.32-1338" },
-                    "167.42.23.32-1338");
-
-                resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1337");
                 Assert.AreEqual("] My P3rfect Server [", resultServer.Name);
                 Assert.AreEqual(new[] { "DM", "TDM" }, resultServer.GameModes);
 
-                resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1338");
+                Classes.Server.AddServerInfo(serversCollection,
+                    new Classes.Server
+                    {
+                        Name = ">> Sniper Heaven <<",
+                        GameModes = new[] { "CtF", "TS", "DM", "TDM" },
+                        Endpoint = "167.42.23.32-1337"
+                    },
+                    "167.42.23.32-1337");
+
+                resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1337");
                 Assert.AreEqual(">> Sniper Heaven <<", resultServer.Name);
-                Assert.AreEqual(gameModes, resultServer.GameModes);
+                Assert.AreEqual(new[] { "CtF", "TS", "DM", "TDM" }, resultServer.GameModes);
             }
         }
+        //[Test]
+        //public static void TestServerAddition2()
+        //{
+        //    using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
+        //    {
+        //        var serversCollection = testDatabase.GetCollection<Classes.Server>("servers");
+        //        serversCollection.EnsureIndex(x => x.Endpoint);
+
+        //        var gameModes = new[] { "DM", "TDM" };
+        //        Classes.Server.AddServerInfo(serversCollection,
+        //            new Classes.Server() { Name = "] My P3rfect Server [", GameModes = gameModes, Endpoint = "167.42.23.32-1337" },
+        //            "167.42.23.32-1337");
+
+        //        var resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1337");
+        //        Assert.AreEqual("] My P3rfect Server [", resultServer.Name);
+        //        Assert.AreEqual(gameModes, resultServer.GameModes);
+
+        //        gameModes = new[] { "CtF", "TS", "DM", "TDM" };
+        //        Classes.Server.AddServerInfo(serversCollection,
+        //            new Classes.Server() { Name = ">> Sniper Heaven <<", GameModes = gameModes, Endpoint = "167.42.23.32-1338" },
+        //            "167.42.23.32-1338");
+
+        //        resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1337");
+        //        Assert.AreEqual("] My P3rfect Server [", resultServer.Name);
+        //        Assert.AreEqual(new[] { "DM", "TDM" }, resultServer.GameModes);
+
+        //        resultServer = serversCollection.FindOne(x => x.Endpoint == "167.42.23.32-1338");
+        //        Assert.AreEqual(">> Sniper Heaven <<", resultServer.Name);
+        //        Assert.AreEqual(gameModes, resultServer.GameModes);
+        //    }
+        //}
     }
 }
--- ServiceTests/GetServerTest.cs
+++ ServiceTests/GetServerTest.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using LiteDB;
+using System.ServiceModel.Web;
 
 namespace Kontur.GameStats.Server.ServiceTests
 {
+    [TestFixture]
     class GetServerTest
     {
+        [SetUp]
+        public static void InsertData()
+        {
+            using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
+            {
+                var serversCollection = testDatabase.GetCollection<Classes.Server>("servers");
+
+                Classes.Server.AddServerInfo(serversCollection,
+                    new Classes.Server
+                    {
+                        Name = "] My P3rfect Server [",
+                        GameModes = new[] { "DM", "TDM" },
+                        Endpoint = "167.42.23.32-1337"
+                    },
+                    "167.42.23.32-1337");
+
+                serversCollection.EnsureIndex(x => x.Endpoint);
+            }
+        }
+
+        [TearDown]
+        public static void DropDatabase()
+        {
+            using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
+            {
+                testDatabase.DropCollection("servers");
+            }
+        }
+
+        [Test]
+        public static void TestServerGetting()
+        {
+            using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
+            {
+                var serversCollection = testDatabase.GetCollection<Classes.Server>("servers");
+                var resultServer = Classes.Server.GetServerInfo(serversCollection, "167.42.23.32-1337");
+                Assert.AreEqual("] My P3rfect Server [", resultServer.Name);
+                Assert.AreEqual(new[] { "DM", "TDM" }, resultServer.GameModes);
+            }
+        }
+
+        [Test]
+        public static void TestServerGettingException()
+        {
+            using (var testDatabase = new LiteDatabase(AppDomain.CurrentDomain.BaseDirectory + "TestStorage.db"))
+            {
+                var serversCollection = testDatabase.GetCollection<Classes.Server>("servers");
+                var exception = Assert.Throws<WebFaultException>(() => Classes.Server.GetServerInfo(serversCollection, "127.0.0.1-8080"));
+                Assert.AreEqual("Not Found", exception.Message);
+                Assert.AreEqual(typeof(WebFaultException), exception.GetType());
+            }
+        }
     }
 }
